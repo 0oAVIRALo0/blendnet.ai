8082494e999bc7584ad94ee027e0e5a6645d21bc
0oAVIRALo0 <aviral21029@iiitd.ac.in>
2024-05-18 00:04:15 +0530

    changed yaml file

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -22,6 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
